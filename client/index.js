249f864cf22b38648e3f14b8215e62721343640c
Shane Devers <robert.a.devers@durham.ac.uk>
2019-03-11 17:22:48 +0000

    Altered way in which ingredients list is passed to server

--- client/index.js
+++ client/index.js
@@ -1,47 +1,10 @@
 document.addEventListener('DOMContentLoaded', async function(event){
     getResults(event);
-    //let response = await fetch('http://127.0.0.1:8090/recipes');
-    //let body = await response.text();
-    //let recipes = JSON.parse(body);
-    //for (let i = 0; i < recipes.length; i++) {
-      //  document.getElementById('recipes').innerHTML += '<div class="card" id="' + recipes[i].title + '"><div class="image"><img src=' + recipes[i].thumbnail + '></div><div class="content"><div class="header">' + recipes[i].title + '</div><div class="description">' + recipes[i].description + //'</div></div><div class="extra content"><span class="right floated">' + recipes[i].date + '</span><span><i class="user icon"></i>' + recipes[i].creator + '</span></div></div>';
-        //document.getElementById('modals').innerHTML += '<div class="ui modal" id="modal'+i+'"><div class="header">'+recipes[i].title+'</div><img class="ui medium image" src="'+recipes[i].thumbnail+'"><div class="scrolling content" id="scroll'+ i+'">';
-        //let scroll = '<p><strong>Ingredients:</strong><br><ul>';
-        //for (let j = 0; j < recipes[i].ingredients.length; j++) {
-         //   scroll += '<li>' + recipes[i].ingredients[j] + '<br></li>';
-        //}
-        //scroll += '</ul></p></div>';
-        //document.getElementById('scroll'+i).innerHTML = scroll;
-        //$(document).ready(function(){document.getElementById(recipes[i].title).addEventListener('click', function(){$('#modal' + i).modal('show');})})
-    //}
     $(document).ready(function(){document.getElementById('NewBtn').addEventListener('click', function(){$('#newRecipe').modal('show');})})
 });
 
 $(document).ready(function(){document.getElementById('search').addEventListener('input', async function(event){
     getResults(event);
-    /*
-    let response = await fetch('http://127.0.0.1:8090/recipes');
-    let body = await response.text();
-    let recipes = JSON.parse(body);
-    let query = document.getElementById('search').value;
-    document.getElementById('recipes').innerHTML = "";
-    document.getElementById('modals').innerHTML = "";
-    for (let i = 0; i < recipes.length; i++) {
-        if (recipes[i].title.toUpperCase().includes(query.toUpperCase())){
-            document.getElementById('recipes').innerHTML += '<div class="card" id="' + recipes[i].title + '"><div class="image"><img src=' + recipes[i].thumbnail + '></div><div class="content"><div class="header">' + recipes[i].title + '</div><div class="description">' + recipes[i].description + '</div></div><div class="extra content"><span class="right floated">' + recipes[i].date + '</span><span><i class="user icon"></i>' + recipes[i].creator + '</span></div></div>';
-            document.getElementById('modals').innerHTML += '<div class="ui modal" id="modal'+i+'"><div class="header">'+recipes[i].title+'</div><img class="ui medium image" src="'+recipes[i].thumbnail+'"><div class="scrolling content" id="scroll'+ i+'">';
-            let scroll = '<p><strong>Ingredients:</strong><br><ul>';
-            for (let j = 0; j < recipes[i].ingredients.length; j++) {
-                scroll += '<li><a id="' + recipes[i].title + j + '">' + recipes[i].ingredients[j] + '</a><br></li>';
-                document.getElementById(recipes[i].title+j).addEventListener('click', async function(event){
-                    
-                })
-            }
-            scroll += '</ul></p></div>';
-            document.getElementById('scroll'+i).innerHTML = scroll;
-            $(document).ready(function(){document.getElementById(recipes[i].title).addEventListener('click', function(){$('#modal' + i).modal('show');})})
-        }
-    }*/
 })});
 
 async function getResults(event) {
@@ -51,7 +14,7 @@ async function getResults(event) {
     let query = document.getElementById('search').value;
     document.getElementById('recipes').innerHTML = "";
     document.getElementById('modals').innerHTML = "";
-    for (let i = 0; i < recipes.length; i++) {
+    for (let i = recipes.length-1; i > -1; i--) {
         if (recipes[i].title.toUpperCase().includes(query.toUpperCase())){
             document.getElementById('recipes').innerHTML += '<div class="card" id="' + recipes[i].title + '"><div class="image"><img src=' + recipes[i].thumbnail + '></div><div class="content"><div class="header">' + recipes[i].title + '</div><div class="description">' + recipes[i].description + '</div></div><div class="extra content"><span class="right floated">' + recipes[i].date + '</span><span><i class="user icon"></i>' + recipes[i].creator + '</span></div></div>';
             document.getElementById('modals').innerHTML += '<div class="ui modal" id="modal'+i+'"><div class="header">'+recipes[i].title+'</div><img class="ui medium image" src="'+recipes[i].thumbnail+'"><div class="scrolling content" id="scroll'+ i+'">';
@@ -70,7 +33,6 @@ async function getResults(event) {
 $(document).ready(function(){document.getElementById('addRecipe').addEventListener('submit', async function(event){
     event.preventDefault();
     try {
-        let newRecipe = {};
         let today = new Date();
         let m = today.getMonth();
         let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
@@ -82,30 +44,15 @@ $(document).ready(function(){document.getElementById('addRecipe').addEventListen
         let title = document.getElementById('RecipeTitle').value;
         let description = document.getElementById('RecipeDescription').value;
         let ingredients = document.getElementById('RecipeIngredients').value;
-        let ingredientList = ingredients.split("\n");
-        let newIngredientList = [];
-        for (let i = 0; i < ingredientList.length; i++) {
-            newIngredientList.push(ingredientList[i]);
-        }
         let thumbnail = document.getElementById('RecipeThumbnail').value;
-        let response = await fetch('http://127.0.0.1:8090/new', 
-                                    {
-                                        method: "POST",
-                                        headers: {
-                                            "Content-Type": "application/x-www-form-urlencoded"
-                                        },
-                                        /*
-                                        body: {"date": date,
-                                            "creator": creator,
-                                            "title": title,
-                                            "description": description,
-                                            "ingredients": newIngredientList,
-                                            "thumbnail": thumbnail
-                                        }
-                                        */
-                                        body: "date=" + date + "&creator=" + creator + "&title=" + title + "&description=" + description + "&ingredients=" + encodeURIComponent(JSON.stringify(newIngredientList)) + "&thumbnail=" + thumbnail
-                                    });
-        console.log(newIngredientList);
+        let response = await fetch('http://127.0.0.1:8090/new', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: "date=" + date + "&creator=" + creator + "&title=" + title + "&description=" + description + "&ingredients=" + ingredients + "&thumbnail=" + thumbnail
+        });
+        getResults(event);
         if (!response.ok) {
             throw new Error("problem adding recipe" + response.code);
         }
--- server.js
+++ server.js
@@ -11,62 +11,13 @@ app.get('/recipes', function(req, resp){
 });
 
 app.post('/new', function(req, resp){
-    /*
-    let newRecipe = {};
-    let today = new Date();
-    let m = today.getMonth();
-    let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    let month = months[m];
-    let day = today.getDate();
-    let year = today.getFullYear();
-    newRecipe.date = day + " " + month + " " + year;
-    newRecipe.creator = "baker213";
-    newRecipe.title = req.body.RecipeTitle;
-    newRecipe.description = req.body.RecipeDescription;
-    let ingredients = req.body.RecipeIngredients;
-    let ingredientList = ingredients.split("\r\n");
-    let newIngredientList = [];
-    for (let i = 0; i < ingredientList.length; i++) {
-        newIngredientList.push(ingredientList[i]);
-    }
-    newRecipe.ingredients = newIngredientList;
-    newRecipe.thumbnail = req.body.RecipeThumbnail;
-    let complete = true;
-    for (const property in newRecipe) {
-        if (newRecipe.hasOwnProperty(property)) {
-            if (newRecipe[property] == "") {
-                complete = false;
-            }
-        }
-    }
-    if (complete) {
-        recipes.push(newRecipe);
-        resp.send(newRecipe);
-    } else {
-        resp.send("Please complete all fields");
-    } 
-    */
-    let ingredients = req.body.ingredients.split(",");
-
-    let ingredientList = [];
-    for (let i = 0; i < ingredients.length; i++) {
-        let currentIngredient = ingredients[i];
-        currentIngredient = currentIngredient.replace("\\","");
-        currentIngredient = currentIngredient.replace('"',"");
-        currentIngredient = currentIngredient.replace('"',"");
-        currentIngredient = currentIngredient.replace("[","");
-        currentIngredient = currentIngredient.replace("]","");
-        console.log(currentIngredient);
-        ingredientList.push(currentIngredient);
-    }
-    console.log(ingredientList);
-    console.log(req.body);
+    let ingredients = req.body.ingredients.split("\n");
     let newRecipe = {
         "date" : req.body.date,
         "creator" : req.body.creator,
         "title" : req.body.title,
         "description" : req.body.description,
-        "ingredients" : ingredientList,
+        "ingredients" : ingredients,
         "thumbnail" : req.body.thumbnail
     };
     recipes.push(newRecipe);
